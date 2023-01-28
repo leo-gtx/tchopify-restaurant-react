6dccdf93dc1313b421e09e5e9b1a7e8197cb5818
Leonel Nguefack <nguefackleonel@gmail.com>
2023-01-28 14:03:18 +0100

    feat/custom-shop

--- src/components/_dashboard/restaurant/RestaurantNewForm.js
+++ src/components/_dashboard/restaurant/RestaurantNewForm.js
@@ -119,7 +119,7 @@ export default function RestaurantNewForm({ isEdit, currentRestaurant }) {
       avatarUrl: isEdit && { preview: currentRestaurant?.image } || {},
       status: isEdit && currentRestaurant?.status || 'activated',
       kmCost: isEdit && currentRestaurant?.kmCost || '',
-      paymentOptions: isEdit && currentRestaurant?.paymentOptions
+      paymentOptions: isEdit && currentRestaurant?.paymentOptions || undefined
     },
     validationSchema: NewRestaurantSchema,
     onSubmit: (values, { setSubmitting, resetForm, setErrors }) => {
